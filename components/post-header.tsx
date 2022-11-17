88c455312fb4af6265f4f892dbdb97bd594e3669
Paolo Vincent Julian <pjulian@yondu.com>
2022-11-17 17:15:45 +0800

    Update header

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -17,10 +17,11 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
     <div className='bg-stone-50 border-b border-stone-200 pt-20 pb-12'>
       <Container>
         <PostTitle>{title}</PostTitle>
+
         <div className='text-center sm:text-left'>
-          <Avatar name={author.name} picture={author.picture} />
-          <div className="text-stone-500 text-base italic">
-            <DateFormatter dateString={date} />
+          {/* <Avatar name={author.name} picture={author.picture} /> */}
+          <div className='text-stone-500 text-base italic'>
+            Published on <DateFormatter dateString={date} />
           </div>
         </div>
       </Container>
