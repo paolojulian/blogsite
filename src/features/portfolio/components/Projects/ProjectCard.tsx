06775b40ff086984175f4ca391acef9dd97759c6
Paolo Vincent Julian <scottypipz@gmail.com>
2023-05-08 15:39:18 +0800

    UI: Projects

--- src/features/portfolio/components/Projects/ProjectCard.tsx
+++ src/features/portfolio/components/Projects/ProjectCard.tsx
@@ -1,15 +1,18 @@
 import classNames from 'classnames';
+import Row from 'layouts/Row';
 import Stack from 'layouts/Stack';
 import React, { FunctionComponent } from 'react';
 
 export type ProjectCardProps = {
   title: string;
   description: string;
+  children?: React.ReactNode;
 };
 
 const ProjectCard: FunctionComponent<ProjectCardProps> = ({
   title,
   description,
+  children,
 }) => {
   return (
     <div
@@ -17,11 +20,15 @@ const ProjectCard: FunctionComponent<ProjectCardProps> = ({
         'h-[350px] w-[475px]',
         'relative',
         'bg-[linear-gradient(207.99deg,_rgba(248,250,252,0.2)_13.77%,rgba(248,250,252,0)_173.77%)]',
-        'border border-gray-600'
+        'border border-white/10'
       )}
     >
+      {/* Contents */}
+      <div className='w-full h-full overflow-x-auto hidden-scrollbar p-[25px]'>
+        <Row className='space-x-4 w-fit h-full flex-nowrap'>{children}</Row>
+      </div>
       {/* Overlay */}
-      <div className='absolute inset-0 w-full h-full bg-gradient-to-t from-bg via-transparent to-transparent'>
+      <div className='absolute inset-0 w-full h-full bg-gradient-to-t from-bg  to-transparent pointer-events-none'>
         <Stack className='justify-end h-full p-4'>
           <h3 className='font-bold text-base'>{title}</h3>
           <p className='font-base text-sm text-slate-400'>{description}</p>
--- src/features/portfolio/components/Projects/index.tsx
+++ src/features/portfolio/components/Projects/index.tsx
@@ -1,9 +1,11 @@
 import DraggableHorizontalScroll from 'components/UI/DraggableHorizontalScroll';
 import Row from 'layouts/Row';
 import Stack from 'layouts/Stack';
+import Image from 'next/image';
 import React, { FunctionComponent } from 'react';
 import SectionHeader from '../common/SectionHeader';
 import ProjectCard from './ProjectCard';
+import ProjectItem from './ProjectItem';
 
 export type ProjectsProps = {
   // No Props
@@ -24,26 +26,198 @@ const Projects: FunctionComponent<ProjectsProps> = () => {
         <DraggableHorizontalScroll>
           <div className='px-4 md:px-12 lg:px-20 xl:px-28'>
             <Row className='space-x-6 w-fit text-white px-4 md:px-12'>
+              {/* GoElect */}
               <ProjectCard
                 title='Election App'
                 description='NextJS, TailwindCSS'
-              />
+              >
+                <>
+                  <ProjectItem
+                    type='web'
+                    width='w-[344px]'
+                    ImageComponent={
+                      <Image
+                        src={
+                          '/assets/portfolio/projects/goelect/web_goelect.jpg'
+                        }
+                        alt={'GoElect'}
+                        width={344}
+                        height={273}
+                        quality={90}
+                      />
+                    }
+                  />
+                  <ProjectItem
+                    type='mobile'
+                    width='w-[138px]'
+                    ImageComponent={
+                      <Image
+                        src={
+                          '/assets/portfolio/projects/goelect/mobile_goelect.jpg'
+                        }
+                        alt={'GoElect'}
+                        width={138}
+                        height={300}
+                        quality={90}
+                      />
+                    }
+                  />
+                  <ProjectItem
+                    type='web'
+                    width='w-[248px]'
+                    ImageComponent={
+                      <Image
+                        src={
+                          '/assets/portfolio/projects/goelect/web_goelect1.jpg'
+                        }
+                        alt={'GoElect'}
+                        width={248}
+                        height={273}
+                        quality={90}
+                      />
+                    }
+                  />
+                </>
+              </ProjectCard>
+              {/* PerformanceDepot */}
               <ProjectCard
                 title='Dynamic Landing Page & Blogsite'
                 description='NextJS, TailwindCSS, Contentful'
-              />
+              >
+                <>
+                  <ProjectItem
+                    type='web'
+                    width='w-[413px]'
+                    ImageComponent={
+                      <Image
+                        src={
+                          '/assets/portfolio/projects/performance_depot/web.jpg'
+                        }
+                        alt={'GoElect'}
+                        width={413}
+                        height={273}
+                        quality={90}
+                      />
+                    }
+                  />
+                  <ProjectItem
+                    type='mobile'
+                    width='w-[138px]'
+                    ImageComponent={
+                      <Image
+                        src={
+                          '/assets/portfolio/projects/performance_depot/mobile.jpg'
+                        }
+                        alt={'GoElect'}
+                        width={138}
+                        height={300}
+                        quality={90}
+                      />
+                    }
+                  />
+                  <ProjectItem
+                    type='web'
+                    width='w-[410px]'
+                    ImageComponent={
+                      <Image
+                        src={
+                          '/assets/portfolio/projects/performance_depot/web2.jpg'
+                        }
+                        alt={'GoElect'}
+                        width={410}
+                        height={273}
+                        quality={90}
+                      />
+                    }
+                  />
+                </>
+              </ProjectCard>
+
+              {/* GoReserve */}
               <ProjectCard
                 title='Reservation App'
                 description='NextJS, TailwindCSS, fabricJS'
-              />
+              >
+                <>
+                  <ProjectItem
+                    type='mobile'
+                    width='w-[138px]'
+                    ImageComponent={
+                      <Image
+                        src={'/assets/portfolio/projects/goreserve/mobile.png'}
+                        alt={'GoElect'}
+                        width={138}
+                        height={300}
+                      />
+                    }
+                  />
+                  <ProjectItem
+                    type='mobile'
+                    width='w-[138px]'
+                    ImageComponent={
+                      <Image
+                        src={'/assets/portfolio/projects/goreserve/mobile2.png'}
+                        alt={'GoElect'}
+                        width={138}
+                        height={300}
+                      />
+                    }
+                  />
+                  <ProjectItem
+                    type='mobile'
+                    width='w-[138px]'
+                    ImageComponent={
+                      <Image
+                        src={'/assets/portfolio/projects/goreserve/mobile3.png'}
+                        alt={'GoElect'}
+                        width={138}
+                        height={273}
+                        quality={90}
+                      />
+                    }
+                  />
+                  <ProjectItem
+                    type='mobile'
+                    width='w-[138px]'
+                    ImageComponent={
+                      <Image
+                        src={'/assets/portfolio/projects/goreserve/mobile4.png'}
+                        alt={'GoElect'}
+                        width={138}
+                        height={273}
+                        quality={90}
+                      />
+                    }
+                  />
+                </>
+              </ProjectCard>
+
+              {/* Leapy */}
               <ProjectCard
                 title='Web Business Management App'
                 description='Laravel, VueJS, Stripe'
-              />
+              >
+                <>
+                  <ProjectItem
+                    type='web'
+                    width='w-[494px]'
+                    ImageComponent={
+                      <Image
+                        src={
+                          '/assets/portfolio/projects/web_business_management/web.jpg'
+                        }
+                        alt={'web_business_management'}
+                        width={494}
+                        height={300}
+                      />
+                    }
+                  />
+                </>
+              </ProjectCard>
               <ProjectCard
                 title='Live Streaming App'
                 description='ReactJS, Laravel, Stripe'
-              />
+              ></ProjectCard>
               <ProjectCard title='Adult Site' description='CakePHP, Stripe' />
             </Row>
           </div>
